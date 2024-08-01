aa89156e32d0f6f1eb94fd6f351401413e2a9ae3
valentino berdoy <berdoy.valentinoet36@gmail.com>
2024-08-01 12:31:29 -0300

    fix(trabajofinal.js):add verificarNombre and fix verificar edad

--- trabajoFinal.js
+++ trabajoFinal.js
@@ -1,39 +1,41 @@
+function verificarNombre(nombre) {
+    if (parseFloat(nombre) === NaN) {
+        console.error("Error: nombre no valido")
+        return false;
+    }
+    if (nombre.length > 10) {
+        console.error("Error: nombre no valido, muy largo")
+        return false;
+    }
+    if (nombre.trim() === '') {
+        console.error("Error: nombre no valido, vacio")
+        return false;
+    }
+    else{
+        return true
+    }
+}
+
 function saludo(nombre){
-    if(nombre != null){
-        console.log(`bienvenido ${nombre.trim()}`);
-        return nombre.trim()
-    }
-    else if(nombre == null || nombre.trim() == ""){
-        console.log("No ingreso nombre lo llamaremos cliente");
-        return "cliente"
-    }
+    console.log(`Bienvenido ${nombre}`)
+    return nombre
 }
 
 function verificarEdad(edad){
     edad = parseFloat(edad);
-    if (typeof edad === "number"){
-        if (edad < 18){
-            console.warn("Usted es menor de edad, recuerde que no podemos venderle bedidas alcoholicas");
-            return false;
-        }
-        else if(edad <= 0 || edad >=100){
-            console.log("edad invalida, lo consideraremos como un menor de edad")
-            return false;
-        }
-        else {
-            console.log("Usted puede consumir bebidas alcoholicas");
-            return true;
-        }
+    if (!isNaN(edad)){  
+        return true;
     }
     else{
-        console.log("no ingreso un numero, lo consideraremos un menor de edad")
+        console.error("Error: dato incorrecto")  
         return false;
     }
 }
 
 function bebidas(edad, cantidadBebidas, bebida){
+    edad = parseFloat(edad);
     if (typeof bebida === "string"){
-        if( edad < 18 && edad >0 || edad >=100){
+        if(edad < 18 && edad >0 || edad >=100){
             switch(bebida.toLowerCase()){
                 case "cerbeza":
                     console.error("Usted es menor no puede pedir alcohol");
@@ -186,29 +188,38 @@ function gestionarPedido(){
     let cantidadBebidas = 0;
     let cantidadPlatos = 0;
     let nombre = prompt("ingrese su nombre")
-    let edad = parseFloat(prompt("ingrese su edad"))
-    let bebida = prompt("elija una bebida, tenemos: cerbeza, cocacola, sprite y agua")
-    if(bebida === "cerbeza" || bebida === "cocacola" || bebida === "sprite" || bebida === "agua"){
-        cantidadBebidas = prompt(`cuantas ${bebida}s quiere?`)
+    if (verificarNombre(nombre)){
+        nombre = (nombre);
+        let edad = prompt("ingrese su edad")
+        if(verificarEdad(edad)){
+            let bebida = prompt("elija una bebida, tenemos: cerbeza, cocacola, sprite y agua")
+            if(bebida === "cerbeza" || bebida === "cocacola" || bebida === "sprite" || bebida === "agua"){
+                cantidadBebidas = prompt(`cuantas ${bebida}s quiere?`)
+            }
+            let plato = prompt("elija un plato, tenemos: ñoquis, ravioles, vacio y sanguche de bondiola")
+            if(plato === "ñoquis"|| plato === "ravioles" || plato === "vacio" || plato === "sangunche de bondiola"){
+                cantidadPlatos= prompt(`cuantas platos de ${plato} quiere?`)
+            }
+            let metodoDePago = prompt("elija el metodo de pago, puede ser: efectivo, tarjeta o aplicacion");
+            const HORA = 150
+            let precioTotal
+            verificarEdad(edad);
+            console.groupCollapsed("Pedido");
+            bebidas(edad, cantidadBebidas, bebida);
+            comida(cantidadPlatos, plato);
+            horaPedido(HORA, nombre)
+            console.groupEnd("Pedido")
+            console.groupCollapsed("Factura")
+            precioTotal = calcularPrecio(cantidadPlatos, plato, cantidadBebidas, bebida, edad, metodoDePago)
+            valorCubierto(precioTotal);
+            console.groupEnd("Factura")
+        }
+        else{
+            return;
+        }
     }
-    let plato = prompt("elija un plato, tenemos: ñoquis, ravioles, vacio y sanguche de bondiola")
-    if(plato === "ñoquis"|| plato === "ravioles" || plato === "vacio" || plato === "sangunche de bondiola"){
-        cantidadPlatos= prompt(`cuantas platos de ${plato} quiere?`)
+    else{
+        return;
     }
-    let metodoDePago = prompt("elija el metodo de pago, puede ser: efectivo, tarjeta o aplicacion");
-    const HORA = 150
-    let precioTotal
-    nombre = saludo(nombre);
-    verificarEdad(edad);
-    console.groupCollapsed("Pedido");
-    bebidas(edad, cantidadBebidas, bebida);
-    comida(cantidadPlatos, plato);
-    horaPedido(HORA, nombre)
-    console.groupEnd("Pedido")
-    console.groupCollapsed("Factura")
-    precioTotal = calcularPrecio(cantidadPlatos, plato, cantidadBebidas, bebida, edad, metodoDePago)
-    valorCubierto(precioTotal);
-    console.groupEnd("Factura")
 }
-
 gestionarPedido();
